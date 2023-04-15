43e5aadb54c116a05c2ff1f75612b267a5e79cc2
Amanda Hope <a5hope@uwaterloo.ca>
2023-04-15 15:04:44 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import useSpeechToText from 'react-hook-speech-to-text';
+//https://github.com/Riley-Brown/react-speech-to-text
+export default function AnyComponent() {
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    googleApiKey: '9d3eda7871a426914b3a1042b35f9d63583c7241',
+    useLegacyResults: false
+});
+
+  if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
+
+  return (
+    <div>
+      <h1>Recording: {isRecording.toString()}</h1>
+      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+        {isRecording ? 'Stop Recording' : 'Start Recording'}
+      </button>
+      <ul>
+        {results.map((result) => (
+          <li key={result.timestamp}>{result.transcript}</li>
+        ))}
+        {interimResult && <li>{interimResult}</li>}
+      </ul>
+    </div>
+  );
+}
---
+++ src/webpack.config.js
@@ -0,0 +1,17 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+
+module.exports = {
+    // Other rules...
+    plugins: [
+        new NodePolyfillPlugin()
+    ],
+// rest of the webpack config
+resolve: {
+    // ... rest of the resolve config
+    fallback: {
+      "path": require.resolve("path-browserify")
+    }
+  }
+}
+
+
